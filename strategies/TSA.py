e88807c1bcb281b0c0fdb7545ce60abaf6b1f950
jiangli <jiangli@gwu.edu>
2021-12-05 06:17:46 +0800

    fixed bugs

--- strategies/TSA.py
+++ strategies/TSA.py
@@ -12,6 +12,8 @@ FILE_PATH = '/root/sampleTest/train/'
 
 file_name = FILE_PATH + '000001.SZ.csv'
 df = pd.read_csv(file_name, parse_dates=True)
+df.set_index('Date', inplace=True)
+df.index = pd.DatetimeIndex(df.index).to_period('M')
 
 # check stationarity with Augmented Dickey-Fuller test
 # The null hypothesis of the test is that the time series is non-stationary
@@ -19,9 +21,9 @@ df = pd.read_csv(file_name, parse_dates=True)
 def tsplot(y, lags=None, figsize=(12, 7), style='bmh'):
     """
         Plot time series, its ACF and PACF, calculate Dickey–Fuller test
-        
-        y - timeseries
-        lags - how many lags to include in ACF, PACF calculation
+    
+        y: timeseries
+        lags: how many lags to include in ACF, PACF calculation
     """
     if not isinstance(y, pd.Series):
         y = pd.Series(y)
@@ -40,21 +42,35 @@ def tsplot(y, lags=None, figsize=(12, 7), style='bmh'):
         smt.graphics.plot_pacf(y, lags=lags, ax=pacf_ax)
         plt.tight_layout()
 
-for col in df.columns:
-    tsplot(df[col], lags=60)
-
-diff = df.diff().dropna()
+# for col in df.columns:
+#     tsplot(df[col], lags=60)
 
 # #fit the model
 def fit_model(df, max_p, columns=None):
-    forecasting_model = VAR(df[columns])
-    results_aic = []
-    for p in range(1, max_p):
-        results = forecasting_model.fit(p)
-        results_aic.append(results.aic)
-        return max(results_aic)
+    """Fit a VAR model
 
-model_result = fit_model(df, 10, ['OpenPrice', 'ClosePrice'])
+    Args:
+        df (pandas DataFrame object): input dataframe
+        max_p (int): largest number to loop through for p
+        columns (list, optional): a list of column names. Defaults to None.
+
+    Returns:
+        VAR model
+    """
+    
+    diff = df[columns].diff().dropna()
+    # can fit multiple columns 
+    forecasting_model = VAR(diff[columns])
+    # try multiple values of p
+    for p in range(1, max_p):
+        result = forecasting_model.fit(p)
+        if p == 1 or result.aic > best_aic:
+            best_aic = result.aic
+            best_result = result
+    return best_result
+
+model_result = fit_model(df, 10, ['OpenPrice', 'ClosePrice', 'HighPrice', 'LowPrice', 'TranscationVolume'])
+print(model_result.summary())
 
 def plot_results():
     sns.set()
